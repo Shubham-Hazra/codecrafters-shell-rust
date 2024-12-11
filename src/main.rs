027bfc16a14079967a5e2f4ac55ad1476c8d4a43
Shubham-Hazra <shubhamhazra1234@gmail.com>
2024-12-11 18:06:20 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,22 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let words: Vec<&str> = input.split_ascii_whitespace().collect();
+        let words: Vec<&str> = input.splitn(2, " ").collect();
 
         if words[0] == "exit" {
-            let exitcode: i32 = words[1].parse().unwrap();
-            process::exit(exitcode);
-        }
+            if words.len() > 2 {
+                println!("Error. Please give an integer exit code");
+                process::exit(1);
+            }
 
-        println!("{}: not found", input.trim());
+            let exitcode: i32 = words[1].trim().parse().unwrap();
+            process::exit(exitcode);
+
+        } else if words[0] == "echo" {
+            print!("{}", words[1]);
+        } else {
+            println!("{}: not found", input.trim());
+        }
     }
 
 }
