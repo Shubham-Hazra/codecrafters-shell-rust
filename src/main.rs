3ca9c8e5316b29a0394184138c917dfbca76789b
Shubham-Hazra <shubhamhazra1234@gmail.com>
2024-12-11 18:14:38 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
+    let builtin_commands: Vec<&str> = vec!["echo", "exit", "type"];
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-    
+
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
@@ -21,12 +23,16 @@ fn main() {
 
             let exitcode: i32 = words[1].trim().parse().unwrap();
             process::exit(exitcode);
-
         } else if words[0] == "echo" {
             print!("{}", words[1]);
+        } else if words[0] == "type" {
+            if builtin_commands.contains(&words[1].trim()) {
+                println!("{} is a shell builtin", words[1].trim())
+            } else {
+                println!("{}: not found", words[1].trim());
+            }
         } else {
             println!("{}: not found", input.trim());
         }
     }
-
 }
